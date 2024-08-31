78418c815a80e170247876b5854a93d421caf59b
“annamariakou” <annamariakou@gmail.com>
2024-08-31 19:12:35 +0100

    adding max roll

---
+++ project.py
@@ -0,0 +1,33 @@
+import random 
+
+def roll():
+    min_value = 1 
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+
+    return roll
+
+value = roll()
+
+while True: 
+    players = input("Enter the number of players (2-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else: print("Must be between 2 and 4 players")
+    else:
+        print("Invalid input. Try again")
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+
+print(player_scores)
+
+while max(player_scores) < max_score:
+    should_rolle= input("Would you like to roll (y)?")
+    if should_roll.lower() != "y":
+        break
+
+    value = roll()
